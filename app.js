2766d08b7e3d04955fe89fc86f985402dbf65707
oscarstorm <oscar.emil.storm@gmail.com>
2023-09-14 22:19:36 +0200

    made fetch quote call

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require("express");
-
 const app = express();
+const PORT = 8080;
+
+let fetch;
+
+import('node-fetch').then(module => {
+  fetch = module.default;
+});
 
 // Create GET request
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/home.html");
 });
 
-const PORT = 8080;
+app.get('/getQuote', async (req, res) => {
+    try {
+      const response = await fetch('https://zenquotes.io/api/random');
+      const data = await response.json();
+      res.json(data);
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  });
+
+
 app.listen(PORT, (error) => {
     if (error) {
         console.log("Server failed to start", error);
